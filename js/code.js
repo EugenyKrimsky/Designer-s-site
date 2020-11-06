17974c750f40ab94ea2d8c82191fd2f1e9d2c11f
EugenyKrimsky <65129303+EugenyKrimsky@users.noreply.github.com>
2020-11-06 16:37:45 +0400

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-    $('a').click(function() {
-      let scrollingTo = $(this).attr('href');
+  $('a').click(function() {
+    let scrollingTo = $(this).attr('href')
+    if (scrollingTo !== '#home') {
       $('html, body').animate({
-          scrollTop: $(scrollingTo).offset().top
+        scrollTop: $(scrollingTo).offset().top
       }, 1000);
-    });
-    $('.burger').click(function(event) {
-        $('.burger,.nav-container').toggleClass('active');
-        $('body').toggleClass('lock')
-    })
-    $('.nav_a').click(function(event) {
-      $('.burger,.nav-container').toggleClass('active');
-      $('body').toggleClass('lock')
-    })
+    }
+    else {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1000);
+    }
+  });
+  $('.burger').click(function(event) {
+    $('.burger,.nav-container').toggleClass('active');
+    $('body').toggleClass('lock')
+  })
+  $('.nav_a').click(function(event) {
+    $('.burger,.nav-container').toggleClass('active');
+    $('body').toggleClass('lock')
+  })
 })
